4e066ac88cffaaa9bb91a7eca5fa742b5c3249e8
coder samuel <samuelsantos.ss95@gmail.com>
2021-06-01 23:43:03 -0300

    Rotas entrar-cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+  {path: 'entrar', component:EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
