c5c64bd4a848bec1e233963fef02ac96e681b30e
coder samuel <samuelsantos.ss95@gmail.com>
2021-06-03 00:23:57 -0300

    Componentização Tema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
-
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
 
   {path: '', redirectTo: 'entrar', pathMatch: 'full'},
   {path: 'entrar', component:EntrarComponent},
   {path: 'cadastrar', component: CadastrarComponent},
-  {path: 'inicio', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent},
+  {path: 'tema', component: TemaComponent}
 ];
 
 @NgModule({
